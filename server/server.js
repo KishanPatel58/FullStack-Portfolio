f7ea53d0398cbc252864bb0a0ff8c576bfb47a60
patelkishan3101@gmail.com <patelkishan3101@gmail.com>
2026-09-24 03:30:11 +0530

    full stack portfolio init

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import server from "./src/app.js"
-import connectDB from "./src/config/db.config.js";
-const port = process.env.PORT;
-
-// Connect Database.
-connectDB()
-
-server.listen(port, ()=>console.log(`Server is Running on PORT: ${port}`))
--- server/src/models/hobby.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const hobbySchema = new Schema({
-    name: String
-},{timestamps:true})
-
-const Hobbies = model("Hobby", hobbySchema);
-
-export default Hobbies;
